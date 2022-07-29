96ac7d8fe16808d70d961479c01722d06987c429
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-07-29 12:32:54 +0300

    add components folder

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Button } from "./Button.css";
+import { Button } from "./Button";
 import { Link } from 'react-router-dom';
 import "./NavBar.css";
 
