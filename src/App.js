9df455da5dea77bda35416b027aa2be0e13c482e
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-03-06 10:12:14 +0000

    edit build for deployment

--- src/App.js
+++ src/App.js
@@ -6,36 +6,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-import {useEffect, useState} from "react"
 
 
 function App() {
-  const [data, setData] =useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('https://travel-videos.herokuapp.com/travelvideos')
-    .then(response =>  {
-      if (response.ok ){
-        return response.json()
-      }
-      throw response;
-    })
-    .then(data => {
-      setData (data);
-    })
-    .catch(error => {
-      console.error("Error fetchin data: ", error);
-      setError(error);
-    })
-    .finally(()=> {
-      setLoading(false);
-    })
-  }, [])
-    
-  if (loading) return "Loading....";
-  if (error) return "Error!";
   return (
     <>
       <Router>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,37 +2,9 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
-// import axios from 'axios';
-import {useEffect, useState} from "react";
-// import { Card } from "react-bootstrap";
+
 
 function HeroSection() {
-  const [data, setData] =useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('https://travel-videos.herokuapp.com/travelvideos')
-    .then(response =>  {
-      if (response.ok ){
-        return response.json()
-      }
-      throw response;
-    })
-    .then(data => {
-      setData (data);
-    })
-    .catch(error => {
-      console.error("Error fetchin data: ", error);
-      setError(error);
-    })
-    .finally(()=> {
-      setLoading(false);
-    })
-  }, [])
-    
-  if (loading) return "Loading....";
-  if (error) return "Error!";
 
   return (
     <div className='hero-container'>
